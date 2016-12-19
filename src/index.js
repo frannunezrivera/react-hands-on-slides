a57664a71b9da65afded7da1c465171d78ddd7a2
Fran Nunez <fran.nunez@rakuten.com>
2016-12-19 09:33:06 +0900

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('prismjs');
+require('prismjs/components/prism-jsx');
+var Reveal = require('../node_modules/reveal');
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    // default/cube/page/concave/zoom/linear/fade/none
+    transition: 'none'
+});
+
